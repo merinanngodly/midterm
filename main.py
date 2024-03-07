db9788dfb3d5528551734821568ddcdceede1feb
merinanngodly <godlym1@southernct.edu>
2024-03-07 02:48:48 +0000

    added addition and multiplication functionality + .flaskenv

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/add/<num1>/<num2>')
+def add(num1,num2):
+    return f'<h1>{num1}+{num2}={str(int(num1)+int(num2))}</h1>'
+
+@app.route('/mult/<num1>/<num2>')
+def mult(num1,num2):
+    return f'<h1>{num1}*{num2}={str(int(num1)*int(num2))}</h2>'
